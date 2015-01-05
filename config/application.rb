7f254c1532d8aedfe17f5faeea2ba5a1f1ba9930
Suketu Parikh <suketuparikh@gmail.com>
2015-01-05 17:44:29 +0530

    updated

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Mystore
   class Application < Rails::Application
-    
+    config.assets.initialize_on_precompile = false
     config.to_prepare do
       # Load application's model / class decorators
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
@@ -21,7 +21,7 @@ module Mystore
       end
     end
 	
-	config.assets.initialize_on_precompile = false
+	
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
