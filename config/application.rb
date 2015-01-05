e61be9a7488ac7ca4c4c744c5dc8c577dce697cf
Suketu Parikh <suketuparikh@gmail.com>
2015-01-05 16:58:38 +0530

    updated application.rb

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.assets.initialize_on_precompile = false
 module Mystore
   class Application < Rails::Application
     
@@ -32,6 +32,5 @@ module Mystore
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-	config.assets.initialize_on_precompile = false
   end
 end
